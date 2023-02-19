75341104097b45943081fd153a28bbdc066043da
Dhaval Vachheta <79516768+Dhaval-Vachheta@users.noreply.github.com>
2023-02-19 12:57:18 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 # Import Libraries
 import pandas as pd
 import numpy as np
-
+print("Hello")
 # # Data Visualizaiton
 # import plotly.express as px
 # import plotly.graph_objects as go
@@ -12,11 +12,11 @@ import numpy as np
 
 # Load CSV Data
 # df = pd.read_csv("../input/kaggle-survey-2022/kaggle_survey_2022_responses.csv")
-url = 'https://github.com/Dhaval-Vachheta/Data-Visualization-Kaggle-Competition-2022/blob/main/Dataset/kaggle_survey_2022_responses.csv'
-df = pd.read_csv(url)
+# url = 'https://github.com/Dhaval-Vachheta/Data-Visualization-Kaggle-Competition-2022/blob/main/Dataset/kaggle_survey_2022_responses.csv'
+# df = pd.read_csv(url)
 
 # Extend Columns In Output
-pd.set_option('display.max_columns', 350)
+# pd.set_option('display.max_columns', 350)
 
 # Display Data
-df.head()
+# df.head()
