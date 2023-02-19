5adeebc040bb072159c63a71c7ce3940b7a90dfa
Dhaval Vachheta <79516768+Dhaval-Vachheta@users.noreply.github.com>
2023-02-19 14:02:12 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -13,17 +13,19 @@ import numpy as np
 
 # TITLE
 # st.title(":blue[Data Visualization]")
-st.markdown("<h1 style = 'text-align : center; color : DodgerBlue;'>Data Visualization</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style = 'text-align : center; color : DodgerBlue;'>Data Visualization</h1>", unsafe_allow_html = True)
 
-# # Load CSV Data
-# # df = pd.read_csv("../input/kaggle-survey-2022/kaggle_survey_2022_responses.csv")
+# Header
+st.subheader("<h1 style = 'text-align : center; color : DodgerBlue;'>Kaggle Survey 2022 Dataset</h1>", unsafe_allow_html = True)
+
+# Load CSV Data
 url = 'https://github.com/Dhaval-Vachheta/Data-Visualization-Kaggle-Competition-2022/blob/main/Dataset/kaggle_survey_2022_responses.csv?raw=true'
 df = pd.read_csv(url)
 
-# # Extend Columns In Output
+# Extend Columns In Output
 pd.set_option('display.max_columns', 350)
 
-# # Display a static table
+# Display a static table
 # st.table(df)
 
 # Display Entire Data
