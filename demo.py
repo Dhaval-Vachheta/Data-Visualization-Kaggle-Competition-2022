6ef19cbebf757a16e40b06363291514f3e385fa6
Dhaval Vachheta <79516768+Dhaval-Vachheta@users.noreply.github.com>
2023-02-19 14:03:05 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -16,7 +16,7 @@ import numpy as np
 st.markdown("<h1 style = 'text-align : center; color : DodgerBlue;'>Data Visualization</h1>", unsafe_allow_html = True)
 
 # Header
-st.subheader("<h1 style = 'text-align : center; color : DodgerBlue;'>Kaggle Survey 2022 Dataset</h1>", unsafe_allow_html = True)
+st.markdown("<h3 style = 'text-align : center; color : DodgerBlue;'>Kaggle Survey 2022 Dataset</h3>", unsafe_allow_html = True)
 
 # Load CSV Data
 url = 'https://github.com/Dhaval-Vachheta/Data-Visualization-Kaggle-Competition-2022/blob/main/Dataset/kaggle_survey_2022_responses.csv?raw=true'
