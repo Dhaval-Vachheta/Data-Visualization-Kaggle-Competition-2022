709c842d5196c18515ba61d77c6e69c9389f1e6c
Dhaval Vachheta <79516768+Dhaval-Vachheta@users.noreply.github.com>
2023-02-19 11:05:14 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,4 +1,22 @@
-import streamlit as st
+# Import Libraries
+import pandas as pd
+import numpy as np
 
-st.title("Data Visualization")
-st.header("Dhaval Vachheta")
+# Data Visualizaiton
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+from matplotlib import pyplot as plt
+%matplotlib inline
+import seaborn as sb
+
+# Load CSV Data
+# df = pd.read_csv("../input/kaggle-survey-2022/kaggle_survey_2022_responses.csv")
+url = 'https://github.com/Dhaval-Vachheta/Data-Visualization-Kaggle-Competition-2022/blob/main/Dataset/kaggle_survey_2022_responses.csv'
+df = pd.read_csv(url)
+
+# Extend Columns In Output
+pd.set_option('display.max_columns', 350)
+
+# Display Data
+df.head()
