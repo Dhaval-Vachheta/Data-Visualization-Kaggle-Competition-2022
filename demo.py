c36e8f0b80062070ffca368a3f57ebfd1f61e3cc
Dhaval Vachheta <79516768+Dhaval-Vachheta@users.noreply.github.com>
2023-02-19 13:01:26 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -4,12 +4,12 @@ import numpy as np
 import streamlit as st
 
 # # Data Visualizaiton
-import plotly.express as px
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-from matplotlib import pyplot as plt
+# import plotly.express as px
+# import plotly.graph_objects as go
+# from plotly.subplots import make_subplots
+# from matplotlib import pyplot as plt
 # %matplotlib inline
-import seaborn as sb
+# import seaborn as sb
 
 # Load CSV Data
 # df = pd.read_csv("../input/kaggle-survey-2022/kaggle_survey_2022_responses.csv")
