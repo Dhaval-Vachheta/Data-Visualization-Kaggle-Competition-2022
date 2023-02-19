ec4751ec2baa92523dc28a12266a7a837782e563
Dhaval Vachheta <79516768+Dhaval-Vachheta@users.noreply.github.com>
2023-02-19 13:04:02 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,23 +1,37 @@
-# Import Libraries
+# # Import Libraries
+# import pandas as pd
+# import numpy as np
+# import streamlit as st
+
+# # # Data Visualizaiton
+# # import plotly.express as px
+# # import plotly.graph_objects as go
+# # from plotly.subplots import make_subplots
+# # from matplotlib import pyplot as plt
+# # %matplotlib inline
+# # import seaborn as sb
+
+# # Load CSV Data
+# # df = pd.read_csv("../input/kaggle-survey-2022/kaggle_survey_2022_responses.csv")
+# url = 'https://github.com/Dhaval-Vachheta/Data-Visualization-Kaggle-Competition-2022/blob/main/Dataset/kaggle_survey_2022_responses.csv'
+# df = pd.read_csv(url)
+
+# # Extend Columns In Output
+# # pd.set_option('display.max_columns', 350)
+
+# # Display Data
+# st.dataframe(df)
+
+import streamlit as st
 import pandas as pd
 import numpy as np
-import streamlit as st
 
-# # Data Visualizaiton
-# import plotly.express as px
-# import plotly.graph_objects as go
-# from plotly.subplots import make_subplots
-# from matplotlib import pyplot as plt
-# %matplotlib inline
-# import seaborn as sb
+df = pd.DataFrame(
+    np.random.randn(10, 5),
+    columns=("col %d" % i for i in range(5)))
 
-# Load CSV Data
-# df = pd.read_csv("../input/kaggle-survey-2022/kaggle_survey_2022_responses.csv")
-url = 'https://github.com/Dhaval-Vachheta/Data-Visualization-Kaggle-Competition-2022/blob/main/Dataset/kaggle_survey_2022_responses.csv'
-df = pd.read_csv(url)
+# Display a static table
+st.table(df)
 
-# Extend Columns In Output
-# pd.set_option('display.max_columns', 350)
-
-# Display Data
+# Display an interactive table
 st.dataframe(df)
