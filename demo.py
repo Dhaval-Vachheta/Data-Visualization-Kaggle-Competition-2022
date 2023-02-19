bc94da1dcaf00cbfff882bdefed41145c2c7b70c
Dhaval Vachheta <79516768+Dhaval-Vachheta@users.noreply.github.com>
2023-02-19 13:11:48 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -14,7 +14,7 @@ import numpy as np
 # # Load CSV Data
 # # df = pd.read_csv("../input/kaggle-survey-2022/kaggle_survey_2022_responses.csv")
 url = 'https://github.com/Dhaval-Vachheta/Data-Visualization-Kaggle-Competition-2022/blob/main/Dataset/kaggle_survey_2022_responses.csv'
-df = pd.read_csv(url,index_col=0)
+df = pd.read_csv(url)
 
 # # Extend Columns In Output
 # # pd.set_option('display.max_columns', 350)
@@ -30,4 +30,4 @@ df = pd.read_csv(url,index_col=0)
 # st.table(df)
 
 # Display an interactive table
-st.dataframe(df)
+st.table(df)
