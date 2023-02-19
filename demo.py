86747bde0ff5410b44f8b23acda09a1410cb497e
Dhaval Vachheta <79516768+Dhaval-Vachheta@users.noreply.github.com>
2023-02-19 13:00:36 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,22 +2,22 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-st.text("Hello")
+
 # # Data Visualizaiton
-# import plotly.express as px
-# import plotly.graph_objects as go
-# from plotly.subplots import make_subplots
-# from matplotlib import pyplot as plt
-# %matplotlib inline
-# import seaborn as sb
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+from matplotlib import pyplot as plt
+%matplotlib inline
+import seaborn as sb
 
 # Load CSV Data
 # df = pd.read_csv("../input/kaggle-survey-2022/kaggle_survey_2022_responses.csv")
-# url = 'https://github.com/Dhaval-Vachheta/Data-Visualization-Kaggle-Competition-2022/blob/main/Dataset/kaggle_survey_2022_responses.csv'
-# df = pd.read_csv(url)
+url = 'https://github.com/Dhaval-Vachheta/Data-Visualization-Kaggle-Competition-2022/blob/main/Dataset/kaggle_survey_2022_responses.csv'
+df = pd.read_csv(url)
 
 # Extend Columns In Output
 # pd.set_option('display.max_columns', 350)
 
 # Display Data
-# df.head()
+st.dataframe(df)
